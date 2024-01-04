b653939a9ab7b96fadf5d5d03fdeec221a9cb1e9
abhi <abhi@example.com>
2024-01-04 09:54:24 +0100

    Chapter 2: returning HTML content from routes.py

--- Flask/microblog/app/routes.py
+++ Flask/microblog/app/routes.py
@@ -1,6 +1,21 @@
 from app import app 
 
+# @app.route("/")
+# @app.route("/index")
+# def index():
+#     return "Hello, World"
+
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World"
+    user = {"username":"Abhilash"}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, '''+user["username"]+'''!</h1>
+        </body>
+    </html>
+    '''
