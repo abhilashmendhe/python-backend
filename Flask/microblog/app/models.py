9fe274436fdb0e78fade27def5f0fa69ff758ecb
abhi <abhi@example.com>
2024-02-28 16:35:25 +0100

    in between chapter 5

--- Flask/microblog/app/models.py
+++ Flask/microblog/app/models.py
@@ -4,6 +4,8 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -15,6 +17,12 @@ class User(db.Model):
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
